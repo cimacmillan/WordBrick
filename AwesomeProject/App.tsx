2f0d0ddec27858fb6af4601dfd1a2e518371714f
Callum 🍺 <callumimacmillan@gmail.com>
2022-03-05 14:33:42 +0000

    React native stuff

--- AwesomeProject/App.tsx
+++ AwesomeProject/App.tsx
@@ -1,22 +1,110 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import * as React from 'react';
+import { ScrollView, Text, View, Image, TextInput, Button, Animated } from 'react-native';
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import { CompositeScreenProps, NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator, NativeStackScreenProps } from '@react-navigation/native-stack';
 
-export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+const Stack = createNativeStackNavigator();
 
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
+type RootStackParamList = {
+    "Home": {},
+    "Profile": {}
+    "Settings": {}
 }
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Welcome' }}
+        />
+        <Stack.Screen name="Profile" component={Profile} />
+        <Stack.Screen name="Settings" component={Settings} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const Profile = ({ navigation }: NativeStackScreenProps<RootStackParamList, "Profile">) => {
+    return (
+        <ScrollView>
+            <Text>Some text</Text>
+
+        </ScrollView>
+    );
+}
+
+const Settings = ({ navigation }: NativeStackScreenProps<RootStackParamList, "Settings">) => {
+    return (
+        <ScrollView>
+            <Text>Some text</Text>
+
+        </ScrollView>
+    );
+}
+
+const HomeScreen = ({ navigation }: NativeStackScreenProps<RootStackParamList, "Home">) => {
+    const fadeAnim = React.useRef(new Animated.Value(0)).current  // Initial value for opacity: 0
+
+    React.useEffect(() => {
+        Animated.timing(
+        fadeAnim,
+        {
+            toValue: 1,
+            duration: 10000,
+            useNativeDriver: true
+        }
+        ).start();
+    }, [fadeAnim]);
+    
+    return (
+        <ScrollView>
+            <Animated.View
+                style={{
+                    opacity: fadeAnim,
+                }}
+            >
+                <Button title="Profile" onPress={() => navigation.navigate("Profile", {})}/>
+                <Button title="Settings" onPress={() => navigation.navigate("Settings", {})}/>
+                <Text>Some text</Text>
+                <View>
+                    <Text>Some more text</Text>
+                    <Image
+                        source={{
+                            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+                        }}
+                        style={{ width: 200, height: 200 }}
+                    />
+                </View>
+                <TextInput
+                    style={{
+                        height: 40,
+                        borderColor: 'gray',
+                        borderWidth: 1
+                    }}
+                    defaultValue="You can type in me"
+                />
+                <Button
+                    onPress={() => {
+                        alert('You tapped the button!');
+                    }}
+                    title="Press Me"
+                    />
+                <Text style={{fontSize: 40}}>Hello, I am your cat!</Text>
+                <Text style={{fontSize: 40}}>Hello, I am your cat!</Text>
+                <Text style={{fontSize: 40}}>Hello, I am your cat!</Text>
+                <Text style={{fontSize: 40}}>Hello, I am your cat!</Text>
+                <Text style={{fontSize: 40}}>Hello, I am your cat!</Text>
+                <Text style={{fontSize: 40}}>Hello, I am your cat!</Text>
+                <Text style={{fontSize: 40}}>Hello, I am your cat!</Text>
+                <Text style={{fontSize: 40}}>Hello, I am your cat!</Text>
+                <Text style={{fontSize: 40}}>Hello, I am your cat!</Text>
+            </Animated.View>
+        </ScrollView>
+    );
+}
+
+export default App;
